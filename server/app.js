64231b03806cba8ba2f6f1b2b2ca084525f2b3d3
Sidhant Chadda <sidhantchadda@gmail.com>
2017-02-20 18:26:53 -0500

    added strict mode

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
@@ -35,40 +37,27 @@ app.get('/', (req, res) => {
 	res.send('hello please login or signup!');
 });
 
-app.post('/signup', passport.authenticate('local-signup', 
-	{
+app.post('/signup', passport.authenticate('local-signup', {
 		successRedirect: '/main',
 		failureRedirect: '/login',
 		failureFlash: true
-	}
-));
+}));
+
 app.post('/login', passport.authenticate('local-login', {
         successRedirect : '/main', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
 }));
-// app.post('/login', passport.authenticate('local-login', {failureRedirect: '/login', failureFlash: true}), 
-// 	function(req, res) {
-// 		User.findOne({ 'local.email' :  req.body.email }, function(err, user) {
-// 		    if (err)
-// 		            return done(err);
-// 	        if (user)
-// 	            res.redirect('/main');
-// 	        else {
-// 	            res.redirect('/login');
-// 	        }
-// 		});
-// 	}
-// );
-app.get('/login', function(req, res) {
+
+app.get('/login', (req, res) => {
 	res.send('failed to login');
 });
 
-app.get('/main', isLoggedIn, function(req, res) {
+app.get('/main', isLoggedIn, (req, res)=> {
 	res.send('logged in succesful\n');
 });
 
-app.post('/logout', function(req, res) {
+app.post('/logout', (req, res) => {
 	req.logout();
 	res.redirect('/');
 });
